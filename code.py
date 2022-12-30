fc3c97a6c8ae0539afec52efeada244eda3a60cb
Hayden <hayden.thornbrue@mykenzie.snhu.edu>
2022-12-29 19:05:02 -0600

    Band_Name_Generator_Python_Project

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator")
-city_input = input("What is the name of the city you grew up in?")
-pet_input = input("What is the name of your pet?")
+city_input = input("What is the name of the city you grew up in?" + " ")
+pet_input = input("What is the name of your pet?" + " ")
 print("Your band name could be called," + " " + city_input + " " + pet_input + "!")
